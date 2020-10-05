5ca4ef6dd3ee5e379c6069d92cd52a78849da410
Brian Adkins <brian@lojic.com>
2020-10-05 09:35:28 -0400

    Add missing schema_migrations insert

--- TodoApp/database/migration-2020-09-22-01.sql
+++ TodoApp/database/migration-2020-09-22-01.sql
@@ -6,4 +6,6 @@ alter table comment add constraint comment_todo_fk
   foreign key (todo_id) references todo (id)
   on delete cascade;
 
+insert into schema_migrations values ('migration-2020-09-22-01.sql');
+
 commit;
